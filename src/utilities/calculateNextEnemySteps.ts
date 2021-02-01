655c69daf9455d24b6cd7ff673ab161c73db32a4
Timo Schmidberger <schmidi.timo@gmail.com>
2021-02-01 21:36:53 +0100

    Refactoring

--- src/utilities/calculateNextEnemySteps.ts
+++ src/utilities/calculateNextEnemySteps.ts
@@ -1,7 +1,6 @@
 import { calculateNextSteps } from './calculateNextSteps'
 
 function calculateNextEnemySteps(square: string[][], isBlackNext: boolean): number[][] | [] {
-    
     return (square.flatMap((row, ir) => row.flatMap((chell, ic) => {
         if(!isBlackNext && /2../.test(chell) || isBlackNext && /1../.test(chell)) {
             return calculateNextSteps(square, ir, ic, [], true)
--- src/utilities/checkCheckmate.ts
+++ src/utilities/checkCheckmate.ts
@@ -1,6 +1,7 @@
 const checkCheckmate = (myPossibleSteps: number[][], possibleEnemyKingSteps: number[][] | undefined, check: boolean): boolean => {
- 
-    if(check && possibleEnemyKingSteps?.filter(kingStep => !myPossibleSteps.find(myStep => kingStep[0] === myStep[0] && kingStep[1] === myStep[1])).length === 0) {
+    const checkIfEnemyKingCanMove = () => possibleEnemyKingSteps?.filter(kingStep => !myPossibleSteps.find(myStep => kingStep[0] === myStep[0] && kingStep[1] === myStep[1])).length === 0
+
+    if(check && checkIfEnemyKingCanMove()) {
         return true
     }   
     return false
