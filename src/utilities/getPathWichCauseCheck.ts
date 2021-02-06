ccd112a4273e8533307177728035a608baf4cc5a
schmidi64 <schmidi.timo@gmail.com>
2021-02-06 15:29:32 +0100

    Fixing bug when checkmate.

--- src/utilities/getPathWichCauseCheck.ts
+++ src/utilities/getPathWichCauseCheck.ts
@@ -5,7 +5,7 @@ function getPathWichCauseCheck (square: string[][], positionEnemyKing: number[],
 
     if(/.3./.test(figureWichCauseCheck) || /.4./.test(figureWichCauseCheck) || /.5./.test(figureWichCauseCheck)) {
         const allMovesFromfigureWichCauseCheck = calculateNextSteps(square, positionFigureWichCauseCheck[0], positionFigureWichCauseCheck[1], [], false).map(steps => steps.filter(step => step[0] !== undefined && step[1] !== undefined))
-        return [...allMovesFromfigureWichCauseCheck.filter(oneDirectionFromfigureWichCauseCheck => oneDirectionFromfigureWichCauseCheck.find(oneStep => oneStep[0] === positionEnemyKing[0] && oneStep[1] === positionEnemyKing[1])).flatMap(obj => obj), [positionFigureWichCauseCheck[0], positionFigureWichCauseCheck[1]]]
+        return [...allMovesFromfigureWichCauseCheck.filter(oneDirectionFromfigureWichCauseCheck => oneDirectionFromfigureWichCauseCheck.find(oneStep => oneStep[0] === positionEnemyKing[0] && oneStep[1] === positionEnemyKing[1])).flatMap(obj => obj), [positionFigureWichCauseCheck[0], positionFigureWichCauseCheck[1]]].filter(step => step === positionEnemyKing)
     } else {
         return [[positionFigureWichCauseCheck[0], positionFigureWichCauseCheck[1]]]
     }    
